100616b37747802f88b3bf446aca5686df9a7e7a
Angelique Barth <angeliqueb71@aol.com>
2018-02-07 23:45:43 -0500

    linked views and path to proper routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: 'home#home_index'
 
   get '/about' => 'about#parallax'
+
   get '/music' => 'music#music'
+
   get '/events' => 'events#upcoming_events'
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
